9ae848e56b47991f53cf18fb3a429c10eb40dc7d
code-sum <yisugyeong@gmail.com>
2025-09-14 15:48:20 +0900

    chore: update page.tsx

--- dobi-app/src/app/page.tsx
+++ dobi-app/src/app/page.tsx
@@ -13,11 +13,14 @@ export default function Home() {
           priority
         />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            어떤 업무를 도와드릴까요?
+          <li className="tracking-[-.01em]">
+            Dobi handles tedious tasks for graduate students.
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            Dobi does not store user data on the server.
+          </li>
+          <li className="tracking-[-.01em]">
+            Dobi guarantees 100% accurate results without hallucinations.
           </li>
         </ol>
 
@@ -35,7 +38,7 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
+            Scrap now
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"

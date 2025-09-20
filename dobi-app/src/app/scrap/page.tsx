7eaa6f3af6e35a76aa5e87b49aaccc63183178b5
code-sum <yisugyeong@gmail.com>
2025-09-20 21:21:27 +0900

    style: edit layout

--- dobi-app/src/app/scrap/page.tsx
+++ dobi-app/src/app/scrap/page.tsx
@@ -2,30 +2,32 @@ import React from 'react';
 
 export default function Scrap() {
   return (
-    <div>
-      <div className="scrap-first-box">
-        <div>
-          <h1>1단계:</h1>
-          <h1>크롤링 하고 싶은 사이트 주소를 입력해주세요</h1>
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <div className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <div className="scrap-first-box">
+          <div>
+            <h1>1단계:</h1>
+            <h1>크롤링 하고 싶은 사이트 주소를 입력해주세요</h1>
+          </div>
+          <div>
+            <button>+</button>
+            <button>-</button>
+          </div>
+          <div>
+            <button>다음</button>
+          </div>
         </div>
-        <div>
-          <button>+</button>
-          <button>-</button>
-        </div>
-        <div>
-          <button>다음</button>
-        </div>
-      </div>
-      <div className="scrap-second-box">
-        <div>
-          <h1>2단계:</h1>
-          <h1>입력하신 사이트에서 어떤 단어들을 찾고 싶은가요?</h1>
-        </div>
-        <div>
-          <textarea></textarea>
-        </div>
-        <div>
-          <button>다음</button>
+        <div className="scrap-second-box">
+          <div>
+            <h1>2단계:</h1>
+            <h1>입력하신 사이트에서 어떤 단어들을 찾고 싶은가요?</h1>
+          </div>
+          <div>
+            <textarea></textarea>
+          </div>
+          <div>
+            <button>다음</button>
+          </div>
         </div>
       </div>
     </div>
